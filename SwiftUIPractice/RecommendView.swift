87837996f1c9669b18d898cf0ff14b7f7ed02390
seogyeong1 <82697951+seogyeong1@users.noreply.github.com>
2024-04-17 18:31:19 +0900

    [REFACTOR] customModifier, property를 통한 view 분류

--- SwiftUIPractice/RecommendView.swift
+++ SwiftUIPractice/RecommendView.swift
@@ -8,43 +8,51 @@
 import SwiftUI
 
 struct RecommendView: View {
-    var body: some View {
+    
+    var yesPicker: some View {
         VStack {
-            Text("등기부등본은 집주인만 발급받을 수 있다?")
-                .font(.title)
-                .foregroundStyle(.white)
-            Text("매일 푸는 금융 퀴즈 290,300명이 참여 중")
-                .foregroundStyle(.gray)
-            HStack {
-                VStack {
-                    Image(systemName: "circle")
-                        .padding()
-                    Text("그렇다")
-                        
-                }
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .bold()
-                .foregroundStyle(.blue)
+            Image(systemName: "circle")
                 .padding()
-                .background(.indigo)
-                .opacity(0.6)
-                
-                VStack {
-                    Image(systemName: "xmark")
-                        .padding()
-                    Text("아니다")
-                        
-                }
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .bold()
-                .foregroundStyle(.pink)
+            Text("그렇다")
+        }
+        .asBlueBackground()
+    }
+    
+    var noPicker: some View {
+        VStack {
+            Image(systemName: "xmark")
                 .padding()
-                .background(.red)
-                .opacity(0.6)
+            Text("아니다")
+        }
+        .asRedBackground()
+    }
+    
+    var titleText: some View {
+        Text("등기부등본은 집주인만 발급받을 수 있다?")
+            .font(.title2)
+            .foregroundStyle(.white)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading, 20)
+    }
+    
+    var statusText: some View {
+        Text("매일 푸는 금융 퀴즈 290,300명이 참여 중")
+            .foregroundStyle(.gray)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading, 20)
+    }
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            titleText
+            statusText
+
+            HStack(spacing: 10) {
+                yesPicker
+                noPicker
             }
         }
         .background(Color("BackColor"))
-       
 
     }
 }
