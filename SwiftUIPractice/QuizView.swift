fb36bdc9950f53e2e67fee08430b44163ac9928f
seogyeong1 <82697951+seogyeong1@users.noreply.github.com>
2024-04-17 18:30:52 +0900

    [REFACTOR] property, func, struct를 통한 view 분류

--- SwiftUIPractice/QuizView.swift
+++ SwiftUIPractice/QuizView.swift
@@ -8,75 +8,80 @@
 import SwiftUI
 
 struct QuizView: View {
+
+    
+    var recommendText: some View {
+        Text("포인트를 더모을 수 있게 맞춤 혜택을 \n추천해드릴까요?")
+            .foregroundStyle(.white)
+            .font(.title2)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading, 20)
+            .padding(.top, 20)
+    }
+    
     var body: some View {
         VStack {
-            Text("포인트를 더모을 수 있게 맞춤 혜택을 \n추천해드릴까요?")
-                .foregroundStyle(.white)
-                .font(.title2)
-                .padding()
-            
-            HStack {
-                Image(systemName: "checkmark.rectangle.stack.fill")
-                    .foregroundColor(.blue)
-                Text("매일 포인트 받는 출석체크 - 퀴즈")
-                    .foregroundStyle(.gray)
-                    
-            }
-            .padding()
-
-            HStack {
-                Image(systemName: "n.circle.fill")
-                    .foregroundColor(.red)
-                Text("새로운 이벤트")
-                    .foregroundStyle(.gray)
-                    
-            }
-            .padding()
-            
-            HStack {
-                Image(systemName: "star.fill")
-                    .foregroundColor(.yellow)
-                Text("미션 추천")
-                    .foregroundStyle(.gray)
-                    
-            }
-            .padding()
+            recommendText
+            selectText(text: "매일 포인트 받는 출석체크 - 퀴즈",
+                       image: "checkmark.rectangle.stack.fill")
+            .foregroundColor(.blue)
+            selectText(text: "새로운 이벤트", image: "n.circle.fill")
+                .foregroundColor(.red)
+            selectText(text: "미션 추천", image: "star.fill")
+                .foregroundColor(.yellow)
+            Spacer()
             
             VStack {
-                
-                Text(" ")
-                    .padding()
-                    .padding()
-                    .padding()
-                    .padding()
-                    .padding()
-                    .padding()
-                    .padding()
-                    .padding()
-                    .padding()
-                    .padding()
-                
-                Text("[선택] 맞춤형 서비스 이용 동의")
-                    .padding()
-                    .foregroundStyle(.gray)
-                Button("동의하기") {
-                    
-                }
-                .padding()
-                .foregroundStyle(.white)
-                .background(.blue)
-                .cornerRadius(8)
-                
-                Text("다음에 하기")
-                    .padding()
-                    .foregroundStyle(.gray)
-                    
+                subText(text: "[선택] 맞춤형 서비스 이용 동의")
+                PointButton()
+                subText(text: "다음에 하기")
             }
-            
         }
         .background(Color("BackColor"))
+        
+    }
+    
+    func subText(text: String) -> some View {
+        Text(text)
+            .padding()
+            .foregroundStyle(.gray)
+    }
+    
+    func selectText(text: String, image: String) -> some View {
+        HStack {
+            Image(systemName: image)
+            Text(text)
+                .foregroundStyle(.gray)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.leading, 20)
+        .padding(.top, 20)
     }
 }
+
+struct PointButton: View {
+    
+    var body: some View {
+        Button("동의하기") {
+            
+        }
+        
+        .foregroundStyle(.white)
+        .padding(.vertical, 10)
+        .frame(maxWidth: .infinity, alignment: .center)
+        
+        .background(.blue)
+        .cornerRadius(8)
+        .padding(.horizontal, 10)
+        
+        
+        
+        
+        
+        
+    }
+}
+
 
 #Preview {
     QuizView()
